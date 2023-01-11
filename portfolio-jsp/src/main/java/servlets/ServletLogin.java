ea72a2fa7c2ea9280f8bb3c9549c870cdbfb010f
Wandersson1111 <wandersson1111@gmail.com>
2023-01-10 21:16:47 -0300

    Logout e encerramento do sistema criados

--- portfolio-jsp/src/main/java/servlets/ServletLogin.java
+++ portfolio-jsp/src/main/java/servlets/ServletLogin.java
@@ -23,7 +23,17 @@ public class ServletLogin extends HttpServlet {
 
 	/* Recebe os dados pela URL em paramentros */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String acao = request.getParameter("acao");
+		
+		if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate(); // Invalida a cessão
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+		}else {
+		
 		doPost(request, response);
+		}
 	}
 
 	/* Recebe os dados enviados por um formulario */
