7ab9f5da19441067c0065034943ded91116c96a6
Wandersson1111 <wandersson1111@gmail.com>
2023-01-27 21:15:05 -0300

    SQL, método e DAO para savar no banco de dados criados.

--- portfolio-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ portfolio-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import dao.DAOUsuarioRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,7 +16,8 @@ import model.ModelLogin;
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    
+    private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();	
+	
     public ServletUsuarioController() {
         
     }
@@ -28,6 +30,8 @@ public class ServletUsuarioController extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
 		
 		String id = request.getParameter("id");
 		String nome = request.getParameter("nome");
@@ -42,8 +46,20 @@ public class ServletUsuarioController extends HttpServlet {
 		modelLogin.setLogin(login);
 		modelLogin.setSenha(senha);
 		
-		RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
-		redireciona.forward(request, response);
+		daoUsuarioRepository.gravarUsuario(modelLogin);
+		
+		request.setAttribute("msg", "Operação realizada com sucesso!");
+		
+		request.setAttribute("modelLogin", modelLogin);
+		
+		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
 		
 	}
 
